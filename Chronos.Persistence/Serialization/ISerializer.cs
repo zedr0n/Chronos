3e56f3b21e4ec08e8e8312ae8a90e7113f52b34d
zedr0n <zedr0nre@gmail.com>
2017-07-05 04:00:17 +0100

    remove redundant xml serializer

--- Chronos.Persistence/Serialization/ISerializer.cs
+++ Chronos.Persistence/Serialization/ISerializer.cs
@@ -8,31 +8,4 @@ namespace Chronos.Persistence.Serialization
 
         T Deserialize<T>(TextReader reader);
     }
-
-    /*public static class SerializerExtensions
-    {
-        /// <summary>
-        /// Serializes the given data object as a string.
-        /// </summary>
-        public static string Serialize<T>(this ISerializer serializer, T data)
-        {
-            using (var writer = new StringWriter())
-            {
-                serializer.Serialize(writer, data);
-                return writer.ToString();
-            }
-        }
-
-        /// <summary>
-        /// Deserializes the specified string into an object of type <typeparamref name="T"/>.
-        /// </summary>
-        /// <exception cref="System.InvalidCastException">The deserialized object is not of type <typeparamref name="T"/>.</exception>
-        public static T Deserialize<T>(this ISerializer serializer, string serialized)
-        {
-            using (var reader = new StringReader(serialized))
-            {
-                return serializer.Deserialize<T>(reader);
-            }
-        }
-    }*/
 }
