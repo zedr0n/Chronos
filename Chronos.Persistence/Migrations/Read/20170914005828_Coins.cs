bbbe4191abb5a22aaaba9510b4145f318d42ac55
zedr0n <zedr0nre@gmail.com>
2017-09-15 18:44:47 +0100

    fix tests

---
+++ Chronos.Persistence/Migrations/Read/20170914005828_Coins.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Chronos.Persistence.Migrations.Read
+{
+    public partial class Coins : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coins",
+                columns: table => new
+                {
+                    Key = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Ticker = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coins", x => x.Key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Key = table.Column<Guid>(type: "BLOB", nullable: false),
+                    MaxSpeed = table.Column<double>(type: "REAL", nullable: false),
+                    OrderNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    PriceAsset = table.Column<Guid>(type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderStatuses",
+                columns: table => new
+                {
+                    Key = table.Column<Guid>(type: "BLOB", nullable: false),
+                    OrderNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    Speed = table.Column<double>(type: "REAL", nullable: false),
+                    Spent = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderStatuses", x => x.Key);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coins");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "OrderStatuses");
+        }
+    }
+}
--- Chronos.Web/Pages/TrackNicehashOrder.cshtml.cs
+++ Chronos.Web/Pages/TrackNicehashOrder.cshtml.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Threading.Tasks;
 using Chronos.Core.Orders.NiceHash.Commands;
+using Chronos.Core.Orders.NiceHash.Projections;
+using Chronos.Core.Orders.NiceHash.Queries;
 using Chronos.Infrastructure.Commands;
+using Chronos.Infrastructure.Queries;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static System.Double;
 
 namespace Chronos.Web.Pages
 {
@@ -12,11 +16,19 @@ namespace Chronos.Web.Pages
         [BindProperty]
         public CreateOrderCommand Command { get; set; } = new CreateOrderCommand();
 
-        private readonly ICommandBus _commandBus;
+        public OrderStatus OrderStatus { get; set; } = new OrderStatus
+        {
+            Speed = NaN,
+            Spent = NaN
+        };
         
-        public TrackNicehashOrderModel(ICommandBus commandBus)
+        private readonly ICommandBus _commandBus;
+        private readonly IQueryProcessor _queryProcessor;
+        
+        public TrackNicehashOrderModel(ICommandBus commandBus, IQueryProcessor queryProcessor)
         {
             _commandBus = commandBus;
+            _queryProcessor = queryProcessor;
         }
         
         public void OnGet()
@@ -32,7 +44,15 @@ namespace Chronos.Web.Pages
 
             Command.TargetId = Guid.NewGuid();
             await _commandBus.SendAsync(Command);
-            return RedirectToPage("/Index");
+
+            var query = new OrderStatusQuery
+            {
+                OrderId = Command.TargetId
+            };
+
+            OrderStatus = _queryProcessor.Process<OrderStatusQuery, OrderStatus>(query);
+            
+            return Page();
         }
     }
 }
