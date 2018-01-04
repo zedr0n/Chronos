4df4d30b10632289ba05b973c319078817ba9f27
zedr0n <zedr0nre@gmail.com>
2018-01-04 00:24:40 +0000

    fix db

---
+++ Chronos.Persistence/Migrations/Read/20180103235233_CoinHistory2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Chronos.Persistence.Migrations.Read
+{
+    public partial class CoinHistory2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CoinHistories",
+                columns: table => new
+                {
+                    Key = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Timeline = table.Column<Guid>(type: "BLOB", nullable: false),
+                    TimestampUtc = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Values = table.Column<string>(type: "TEXT", nullable: true),
+                    Version = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CoinHistories", x => x.Key);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CoinHistories");
+        }
+    }
+}
