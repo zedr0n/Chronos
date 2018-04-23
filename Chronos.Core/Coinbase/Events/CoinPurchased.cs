7563aa4f3fdc02200c5ede519617f03fbdf02eac
zedr0n <zedr0nre@gmail.com>
2018-04-23 01:10:40 +0100

    CHRONOS-5 fix build

---
+++ Chronos.Core/Coinbase/Events/CoinPurchased.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Chronos.Infrastructure.Events;
+
+namespace Chronos.Core.Coinbase.Events
+{
+    public class CoinPurchased : EventBase
+    {
+        public Guid AccountId { get; }
+        public Guid PurchaseId { get; }
+        public string Coin { get; }
+        public double Quantity { get; }
+        public double CostPerUnit { get; } // in GBP
+        public double Fee { get; }
+
+        public CoinPurchased(Guid accountId, Guid purchaseId, string coin, double quantity, double costPerUnit, double fee)
+        {
+            AccountId = accountId;
+            PurchaseId = purchaseId;
+            Coin = coin;
+            Quantity = quantity;
+            CostPerUnit = costPerUnit;
+            Fee = fee;
+        }
+    }
+}
